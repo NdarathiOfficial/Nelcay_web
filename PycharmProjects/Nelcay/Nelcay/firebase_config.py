14c26743b83de09167963e153669a29b573b3917
NdarathiOficial <ndarathiofficial@gmail.com>
2026-06-16 16:01:23 +0300

    Deployment changes

--- PycharmProjects/Nelcay/Nelcay/firebase_config.py
+++ PycharmProjects/Nelcay/Nelcay/firebase_config.py
@@ -1,22 +1,11 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
 import os
+import json
+import firebase_admin
+from firebase_admin import credentials
 
+# Fetch the JSON string from Environment Variables
+firebase_json_str = os.environ.get('FIREBASE_SERVICE_ACCOUNT_JSON')
+firebase_dict = json.loads(firebase_json_str)
 
-def get_db():
-    if not firebase_admin._apps:
-        # 1. Point to the EXACT file in the Nelcay folder
-        # Ensure 'serviceAccountKey.json' is inside the Nelcay folder (as shown in your screenshot)
-        key_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'serviceAccountKey.json')
-
-        print(f"DEBUG: Reading credentials from: {key_path}")
-
-        # 2. Initialize with absolute certainty
-        cred = credentials.Certificate(key_path)
-        firebase_admin.initialize_app(cred)
-        print("DEBUG: Firebase initialized successfully.")
-
-    return firestore.client()
-
-
-db = get_db()
+cred = credentials.Certificate(firebase_dict)
+firebase_admin.initialize_app(cred)
